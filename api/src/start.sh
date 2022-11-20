fd4abc3f4ba8010ee8a07eb79d4394d034dc5533
Valentin Harapu <alexandru.harapu@gmail.com>
2022-11-20 11:22:54 +0200

    Add Deno api

---
+++ api/src/start.sh
@@ -0,0 +1,6 @@
+#  TODO - > move this file up one dir
+mkdir -p "./../DENO_DIR"
+export DENO_DIR="./../DENO_DIR"
+deno run --allow-net ./server.ts
+# TODO -> make it point to the config file
+# deno run --allow-net --config ../deno.json ./server.ts
